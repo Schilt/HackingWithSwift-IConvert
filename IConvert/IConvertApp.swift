0383b494a78e13d6eb996749dc5048c5c041b325
Andrew A. Schilt <Software.Intelligence@gmail.com>
2022-06-21 22:01:32 -0400

    Initial Commit

---
+++ IConvert/IConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  IConvertApp.swift
+//  IConvert
+//
+//  Created by Andrew A. Schilt on 6/21/22.
+//
+
+import SwiftUI
+
+@main
+struct IConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
